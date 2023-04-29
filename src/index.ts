7bd8300ffbead34a042768fe116237277d34fc77
femsworld <femsworld@gmail.com>
2023-04-29 16:18:59 +0300

    Sync fork

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,28 @@
 import axios from 'axios';
 
-const fetchCompanies = async (max:number, postcode:string, from: string) => {
-    // const result = await get(`https://avoindata.prh.fi/bis/v1?totalResults=false&maxResults=${max}&resultsFrom=${from}&streetAddressPostCode=${postcode}`)
-    const result = await axios(`https://avoindata.prh.fi/bis/v1?totalResults=false&maxResults=${max}&resultsFrom=${from}&streetAddressPostCode=${postcode}`)
-    const data = result.data
-    findComFunc()
-    return data
+// const fetchCompanies = async (max:number, postcode:string, from: string) => {
+//     // const result = await get(`https://avoindata.prh.fi/bis/v1?totalResults=false&maxResults=${max}&resultsFrom=${from}&streetAddressPostCode=${postcode}`)
+//     const result = await axios.get(`https://avoindata.prh.fi/bis/v1?totalResults=false&maxResults=${max}&resultsFrom=${from}&streetAddressPostCode=${postcode}`)
+//     const data = result.data
+//     findComFunc()
+//     return data
+// }
+// const findComFunc = () => {}
+// const companies = fetchCompanies(2, "70100", "2022-09-15")
+// console.log(companies)
+interface Companies {
+    
 }
-const findComFunc = () => {}
-const companies = fetchCompanies(2, "70100", "2020-09-15")
-console.log(companies)
+
+const fetchCompanies = async (max:number, from: string, postCode:string) => {
+    // const data = await axios.get(`https://avoindata.prh.fi/bis/v1?totalResults=false&maxResults=${max}&resultsFrom=${from}&streetAddressPostCode=${postcode}`)
+    const data = await fetch(`https://avoindata.prh.fi/bis/v1?totalResults=false&maxResults=${max}&resultsFrom=${from}&streetAddressPostCode=${postCode}`)
+    const result = data.json()
+    console.log(result)
+    return result
+    // smallFunc()
+}
+// const smallFunc = () => {}
+// const companies = fetchCompanies(30, "02700", "2020-09-15")
+// console.log(companies)   
+fetchCompanies(5, "2020-09-15", "70100", );
