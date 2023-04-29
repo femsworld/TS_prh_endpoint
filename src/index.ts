9a2e61f764a4606fe0bf1ee536f555a0354fbc7a
femsworld <femsworld@gmail.com>
2023-04-29 17:32:43 +0300

    Added some interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { Axios, AxiosError } from 'axios';
 
 // const fetchCompanies = async (max:number, postcode:string, from: string) => {
 //     // const result = await get(`https://avoindata.prh.fi/bis/v1?totalResults=false&maxResults=${max}&resultsFrom=${from}&streetAddressPostCode=${postcode}`)
@@ -10,17 +10,102 @@ import axios from 'axios';
 // const findComFunc = () => {}
 // const companies = fetchCompanies(2, "70100", "2022-09-15")
 // console.log(companies)
-interface Companies {
-    
-}
 
-const fetchCompanies = async (max:number, from: string, postCode:string) => {
-    // const data = await axios.get(`https://avoindata.prh.fi/bis/v1?totalResults=false&maxResults=${max}&resultsFrom=${from}&streetAddressPostCode=${postcode}`)
-    const data = await fetch(`https://avoindata.prh.fi/bis/v1?totalResults=false&maxResults=${max}&resultsFrom=${from}&streetAddressPostCode=${postCode}`)
-    const result = data.json()
-    console.log(result)
-    return result
-    // smallFunc()
+interface Business {
+    businessId: string;
+    name: string;
+    registrationDate: string;
+    companyForm: string;
+    detailsUri: string | null;
+    liquidations: any[];
+    names: Name[];
+    auxiliaryNames: AuxiliaryName[];
+    addresses: Address[];
+    companyForms: CompanyForm[];
+    businessLines: any[];
+    languages: any[];
+    registeredOffices: RegisteredOffice[];
+    contactDetails: any[];
+    registeredEntries: RegisteredEntry[];
+    businessIdChanges: any[];
+  }
+  
+  interface Name {
+    order: number;
+    version: number;
+    name: string;
+    registrationDate: string;
+    endDate: string | null;
+    source: number;
+  }
+  
+  interface AuxiliaryName {
+    order: number;
+    version: number;
+    name: string;
+    registrationDate: string;
+    endDate: string | null;
+    source: number;
+  }
+  
+  interface Address {
+    careOf: string | null;
+    street: string;
+    postCode: string;
+    type: number;
+    version: number;
+    city: string;
+    country: string | null;
+    registrationDate: string;
+    endDate: string | null;
+    language: string;
+    source: number;
+  }
+  
+  interface CompanyForm {
+    version: number;
+    name: string;
+    type: string | null;
+    registrationDate: string;
+    endDate: string | null;
+    language: string;
+    source: number;
+  }
+  
+  interface RegisteredOffice {
+    order: number;
+    version: number;
+    name: string;
+    registrationDate: string;
+    endDate: string | null;
+    language: string;
+    source: number;
+  }
+  
+  interface RegisteredEntry {
+    authority: number;
+    register: number;
+    status: number;
+    registrationDate: string;
+    endDate: string | null;
+    statusDate: string;
+    language: string;
+    description: string;
+  }
+  
+
+const fetchCompanies = async <T>(maxResults :number, resultFrom: string, streetAddressPostCode:string) => {
+    try {
+        const result = await axios<T>(`https://avoindata.prh.fi/bis/v1?totalResults=false&maxResults=${maxResults}&resultsFrom=${resultFrom}&streetAddressPostCode=${streetAddressPostCode}`)
+        return result.data
+        
+    } catch (e) {
+        const error = e as AxiosError //casting the error type
+        console.log(error.message)
+    }
+
+    
+ 
 }
 // const smallFunc = () => {}
 // const companies = fetchCompanies(30, "02700", "2020-09-15")
