9afdd114c27818d211fa595234c6fed7c48ad702
femsworld <femsworld@gmail.com>
2023-04-30 23:51:00 +0300

    Removed comment and white spaces

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,36 @@
-import axios, { Axios, AxiosError } from 'axios';
-import Business  from "./types";
+import axios, { Axios, AxiosError } from "axios";
+import Business from "./types";
 import businessSearch from "./types";
 
-  type businessSearchArray = businessSearch[]
-  type businessIdSearchArray = Business[]
+type businessSearchArray = businessSearch[];
+type businessIdSearchArray = Business[];
 
-const fetchCompanies = async (maxResults :number, resultFrom: number, streetAddressPostCode:string) => {
-    try {
-        const result = await axios(`https://avoindata.prh.fi/bis/v1?totalResults=false&maxResults=${maxResults}&resultsFrom=${resultFrom}&streetAddressPostCode=${streetAddressPostCode}`)
-        let businessSearchArrayResult =  result.data.results as businessSearchArray
-        let resultOfSearchBusiness = [] as businessIdSearchArray
-        for(let item of businessSearchArrayResult){
-        try {
-            const businessId_v1 = await axios(item.detailsUri)
-            // console.log(businessId_v1.data.results)
-            // businessId_v1.data.result.Pu
-            resultOfSearchBusiness = [...resultOfSearchBusiness, ...businessId_v1.data.results]
-        } catch (error) {
-            
-        }
-        }
-        // console.log(resultOfSearchBusiness)
-        // return businessSearchArrayResult
-        return resultOfSearchBusiness
-    } catch (e) {
-        const error = e as AxiosError //casting the error type
-        console.log(error.message)
+const fetchCompanies = async (
+  maxResults: number,
+  resultFrom: number,
+  streetAddressPostCode: string
+) => {
+  try {
+    const result = await axios(
+      `https://avoindata.prh.fi/bis/v1?totalResults=false&maxResults=${maxResults}&resultsFrom=${resultFrom}&streetAddressPostCode=${streetAddressPostCode}`
+    );
+    let businessSearchArrayResult = result.data.results as businessSearchArray;
+    let resultOfSearchBusiness = [] as businessIdSearchArray;
+    for (let item of businessSearchArrayResult) {
+      try {
+        const businessId_v1 = await axios(item.detailsUri);
+        resultOfSearchBusiness = [
+          ...resultOfSearchBusiness,
+          ...businessId_v1.data.results,
+        ]; //spread operator
+      } catch (error) {}
     }
-
-}
-fetchCompanies(3, 0, "70100").then(data => {
-    console.log(data)
-})
-
-
+    return resultOfSearchBusiness;
+  } catch (e) {
+    const error = e as AxiosError; //casting the error type
+    console.log(error.message);
+  }
+};
+fetchCompanies(3, 0, "70100").then((data) => {
+  console.log(data);
+});
--- src/types.ts
+++ src/types.ts
@@ -1,89 +1,89 @@
 export default interface Business {
-    businessId: string;
-    name: string;
-    registrationDate: string;
-    companyForm: string;
-    detailsUri: string;
-    liquidations: any[];
-    names?: Name[];
-    auxiliaryNames?: AuxiliaryName[];
-    addresses?: Address[];
-    companyForms?: CompanyForm[];
-    businessLines?: any[];
-    languages?: any[];
-    registeredOffices?: RegisteredOffice[];
-    contactDetails?: any[];
-    registeredEntries?: RegisteredEntry[];
-    businessIdChanges?: any[];
-  }
-  
-  interface Name {
-    order: number;
-    version: number;
-    name: string;
-    registrationDate: string;
-    endDate: string | null;
-    source: number;
-  }
-  
-  interface AuxiliaryName {
-    order: number;
-    version: number;
-    name: string;
-    registrationDate: string;
-    endDate: string | null;
-    source: number;
-  }
-  
-  interface Address {
-    careOf: string | null;
-    street: string;
-    postCode: string;
-    type: number;
-    version: number;
-    city: string;
-    country: string | null;
-    registrationDate: string;
-    endDate: string | null;
-    language: string;
-    source: number;
-  }
-  
-  interface CompanyForm {
-    version: number;
-    name: string;
-    type: string | null;
-    registrationDate: string;
-    endDate: string | null;
-    language: string;
-    source: number;
-  }
-  
-  interface RegisteredOffice {
-    order: number;
-    version: number;
-    name: string;
-    registrationDate: string;
-    endDate: string | null;
-    language: string;
-    source: number;
-  }
-  
-  interface RegisteredEntry {
-    authority: number;
-    register: number;
-    status: number;
-    registrationDate: string;
-    endDate: string | null;
-    statusDate: string;
-    language: string;
-    description: string;
-  }
-  
-  export default interface businessSearch {
-    businessId: string
-    name: string
-    registrationDate: string
-    companyForm: string
-    detailsUri: string;
-  }
+  businessId: string;
+  name: string;
+  registrationDate: string;
+  companyForm: string;
+  detailsUri: string;
+  liquidations: any[];
+  names?: Name[];
+  auxiliaryNames?: AuxiliaryName[];
+  addresses?: Address[];
+  companyForms?: CompanyForm[];
+  businessLines?: any[];
+  languages?: any[];
+  registeredOffices?: RegisteredOffice[];
+  contactDetails?: any[];
+  registeredEntries?: RegisteredEntry[];
+  businessIdChanges?: any[];
+}
+
+interface Name {
+  order: number;
+  version: number;
+  name: string;
+  registrationDate: string;
+  endDate: string | null;
+  source: number;
+}
+
+interface AuxiliaryName {
+  order: number;
+  version: number;
+  name: string;
+  registrationDate: string;
+  endDate: string | null;
+  source: number;
+}
+
+interface Address {
+  careOf: string | null;
+  street: string;
+  postCode: string;
+  type: number;
+  version: number;
+  city: string;
+  country: string | null;
+  registrationDate: string;
+  endDate: string | null;
+  language: string;
+  source: number;
+}
+
+interface CompanyForm {
+  version: number;
+  name: string;
+  type: string | null;
+  registrationDate: string;
+  endDate: string | null;
+  language: string;
+  source: number;
+}
+
+interface RegisteredOffice {
+  order: number;
+  version: number;
+  name: string;
+  registrationDate: string;
+  endDate: string | null;
+  language: string;
+  source: number;
+}
+
+interface RegisteredEntry {
+  authority: number;
+  register: number;
+  status: number;
+  registrationDate: string;
+  endDate: string | null;
+  statusDate: string;
+  language: string;
+  description: string;
+}
+
+export default interface businessSearch {
+  businessId: string;
+  name: string;
+  registrationDate: string;
+  companyForm: string;
+  detailsUri: string;
+}
